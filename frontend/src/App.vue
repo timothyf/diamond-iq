054baf798a17cf0f7ca4e4c474e488e1156ffb9b
Timothy Fisher <timothyf@gmail.com>
2026-07-15 20:07:34 -0400

    Set app logo in the header

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,11 +1,14 @@
 <script setup>
 import PlayerSearch from './components/PlayerSearch.vue'
+import diamondIqLogo from './assets/diamondiq_logo.png'
 </script>
 
 <template>
   <header class="app-bar">
     <RouterLink class="app-brand" to="/">
-      <span class="app-brand__mark">DIQ</span>
+      <span class="app-brand__mark" aria-hidden="true">
+        <img :src="diamondIqLogo" alt="" />
+      </span>
       <span>
         <strong>DiamondIQ</strong>
         <small>Baseball intelligence</small>
